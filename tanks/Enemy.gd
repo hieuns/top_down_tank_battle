c48e8cd3c1ea1f6db147aab9082ee22cc27f459e
Nguyen Sy Hieu <hieuns.a2@gmail.com>
2018-04-27 16:53:23 +0700

    Initial commit

---
+++ tanks/Enemy.gd
@@ -0,0 +1,36 @@
+extends "res://tanks/Tank.gd"
+
+onready var parent = get_parent()
+
+export (float) var turret_speed
+export (int) var detect_radius
+
+var target = null
+
+func _ready():
+	var circle = CircleShape2D.new()
+	$DetectRadius/CollisionShape2D.shape = circle
+	$DetectRadius/CollisionShape2D.shape.radius = detect_radius
+
+func _process(delta):
+	if target:
+		var target_direction = (target.global_position - global_position).normalized()
+		var current_direction = Vector2(1, 0).rotated($Turret.global_rotation)
+		$Turret.global_rotation = current_direction.linear_interpolate(target_direction, turret_speed * delta).angle()
+		if target_direction.dot(current_direction) > 0.9:
+			shoot()
+
+func control(delta):
+	if parent is PathFollow2D:
+		parent.set_offset(parent.get_offset() + speed * delta)
+		position = Vector2()
+	else:
+		pass
+
+func _on_DetectRadius_body_entered(body):
+	target = body
+
+
+func _on_DetectRadius_body_exited(body):
+	if body == target:
+		target = null
